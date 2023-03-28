1c9577a3a19efe0f6412a4a9943811b8183ed912
Jan Kaifer <jan@kaifer.cz>
2023-03-28 18:03:36 +0200

    don't do vercel/otel

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,7 @@
 export function register() {
   console.log("runtime:", process.env.NEXT_RUNTIME);
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    require("./instrumentation-node");
+    // require("./instrumentation-node");
     console.log("Registered OpenTelemetry for Node.js");
   }
 }
