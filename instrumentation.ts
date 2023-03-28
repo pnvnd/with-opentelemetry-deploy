1b8d37eea6bab337496a2a38caabe2e5fc8aa7c0
Jan Kaifer <jan@kaifer.cz>
2023-03-28 16:49:37 +0200

    revert to vercel otel being usable just in node

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,7 @@
 export function register() {
   if (process.env.NEXT_RUNTIME === "node") {
-    const { registerOTel } = require("@vercel/otel/dist/index.node");
+    const { registerOTel } = require("@vercel/otel");
     registerOTel("with-opentelemetry-deploy");
+    console.log("Registered OpenTelemetry for Node.js");
   }
 }
