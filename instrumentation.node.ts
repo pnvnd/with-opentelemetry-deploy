1be19aad68a34fc2132395ee09f79c3eb7c3368b
Jan Kaifer <jan@kaifer.cz>
2023-03-29 14:24:45 +0200

    migrate to NodeSDK

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,19 +1,14 @@
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import {
-  BasicTracerProvider,
-  ConsoleSpanExporter,
-  SimpleSpanProcessor,
-} from "@opentelemetry/sdk-trace-base";
+// import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
+import { NodeSDK } from "@opentelemetry/sdk-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 
-const provider = new BasicTracerProvider({
+const sdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: "next-app",
   }),
+  traceExporter: new OTLPTraceExporter(),
 });
 
-provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter()));
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-
-provider.register();
+sdk.start();
