fd5bce0a25397638f3b73372505cf451687bb572
Jan Kaifer <jan@kaifer.cz>
2023-03-29 11:42:17 +0200

    update vercel/otel

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,9 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  registerOTel("with-opentelemetry-deploy");
+  console.log("running register");
+  const val = registerOTel("with-opentelemetry-deploy");
+  // const val = null;
+  console.log("finished running register");
+  console.log("segistered otel", val);
 }
