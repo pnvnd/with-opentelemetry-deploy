d5bd9704a5a0e2db9273c77a7e89ca6786d8afea
Peter Nguyen <peter@datacrunch.ca>
2023-03-29 13:27:35 -0400

    Update instrumentation.node.ts

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -2,11 +2,11 @@ import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 // import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 
 const sdk = new NodeSDK({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "next-app",
+    [SemanticResourceAttributes.SERVICE_NAME]: "next-app-http",
   }),
   traceExporter: new OTLPTraceExporter(),
 });
